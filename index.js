d8c6b0be93804c32e663b4b5e6d4ec780c6df183
solalll <solal.pouvillon8@gmail.com>
2025-04-08 12:50:02 +0200

    sprint2

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
 const joueurs = [
-    {
-      nom: "Solal",
-      age: "18",
-      poste: "5",
-    },
-    {
-      nom: "Remi",
-      age: "17",
-      poste: "2",
-    },
-    {
-      nom: "Titouan",
-      age: "17",
-      poste: "3",
-    },
-    {
-      nom: "Martin",
-      age: "19",
-      poste: "1",
-    },
-    {
-      nom: "Nouss",
-      age: "16",
-      poste: "1",
-    },
-  ];
+  {
+    nom: "Solal",
+    age: "18",
+    poste: "5",
+  },
+  {
+    nom: "Remi",
+    age: "17",
+    poste: "2",
+  },
+  {
+    nom: "Titouan",
+    age: "17",
+    poste: "3",
+  },
+  {
+    nom: "Martin",
+    age: "19",
+    poste: "1",
+  },
+  {
+    nom: "Nouss",
+    age: "16",
+    poste: "1",
+  },
+];
 
-  const joueursDiv = document.querySelector(".joueurs");
+const joueursDiv = document.querySelector(".joueurs");
 
-  joueurs.map(({ nom, age, poste }) => {
-    const div = `<div class="joueur"><p>Nom: ${nom}</p><p>Poste: ${poste}</p>\n<p>Age: ${age}</p></div>`;
+joueurs.forEach(({ nom, age, poste }) => {
+  const div = `
+    <div class="joueur">
+      <p><strong>Nom:</strong> <span>${nom}</span></p>
+      <p><strong>Poste:</strong> <span>${poste}</span></p>
+      <p><strong>Age:</strong> <span>${age}</span></p>
+    </div>
+  `;
 
-    joueursDiv.innerHTML += div;
-  });
+  joueursDiv.innerHTML += div;
+});
